5fd09154e3a60959f435dfcfb70b4a4ebb66afa4
Jundullah <65699305+Jundullah-IA@users.noreply.github.com>
2021-03-24 18:45:59 +0700

    Add latihan 3

--- Prak.Sistem_1811081023/src/Modul2_1811081023/Lat3Modul2_1811081023.java
+++ Prak.Sistem_1811081023/src/Modul2_1811081023/Lat3Modul2_1811081023.java
@@ -5,10 +5,13 @@
  */
 package Modul2_1811081023;
 
-import java.io.FileInputStream;
+import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Scanner;
 
 /**
@@ -17,46 +20,55 @@ import java.util.Scanner;
  */
 public class Lat3Modul2_1811081023 {
     
-    public static void main(String[] args) throws FileNotFoundException, IOException {
-        int selection;
-        Scanner input = new Scanner(System.in);
-
-        /***************************************************/
+    public static void main(String[] args) throws FileNotFoundException, IOException  {
+         int selection;
+         String userInput;
+         Scanner input = new Scanner(System.in);
+         String pet1811081023 = "File_1811081023/WriteFile_1811081023.txt";
 
         System.out.println("Menu :");
         System.out.println("-------------------------\n");
         System.out.println("1 - Menulis dengan stream");
         System.out.println("2 - Menulis dengan Filewriter/BufferedWriter");
-        System.out.println("3 - Exit \n\n");
+        System.out.println("3 - Exit \n");
 
-        System.out.print("Please input your selection :");
+        System.out.print("Please input your selection : ");
         selection = input.nextInt();
-        System.out.println("User choice : " + selection);
+        System.out.println("User choice : " + selection + "\n");
         
-        switch(selection) {
-            case 1 : {
-                InputStream file1811081023 = new FileInputStream("File_1811081023/ReadFile_1811081023.txt");
-                int data1811081023 = file1811081023.read();
-                System.out.println(data1811081023);
+         switch(selection) {             
+             case 1 : {
+                OutputStream outputStream = new FileOutputStream(pet1811081023);
 
-                while (data1811081023 != -1) {            
-        //            System.out.print((char) data1811081023);
-                    data1811081023 = file1811081023.read();
-
-                    System.out.println(data1811081023);
-                } System.out.println("");
-
-                file1811081023.close();
-            } break;
-            
-            case 2 : {
+                System.out.println("Open " + pet1811081023 + " for write");
+                System.out.print("Input text ro write : ");
+                userInput = input.nextLine();
+                userInput = input.nextLine();
                 
-            } break;
-                
-            case 3 : {
-                System.out.println("System end");
-            } break;
+                for (int i = 0; i < userInput.length(); i++) {
+                    outputStream.write((byte) userInput.charAt(i));
+                } outputStream.close();
+
+                System.out.println("Output stream closed . . .");
+             } break;
             
-        }
+             case 2 : {
+                File file = new File(pet1811081023);
+                FileWriter writer = new FileWriter(file);
+                BufferedWriter bufferedWriter = new BufferedWriter(writer);
+                
+                System.out.println("Open " + pet1811081023 + " for write");
+                System.out.print("Input text ro write : ");
+                userInput = input.nextLine();
+                userInput = input.nextLine();
+
+                bufferedWriter.write(userInput);
+                bufferedWriter.close();
+             } break;
+                
+             case 3 : {
+                 System.out.println("System end");
+             } break;
+         }
     }
 }
